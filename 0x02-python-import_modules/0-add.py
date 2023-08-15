d38536e34e62e40f90cb4b72968f85437bce89df
Mally13 <mallymutuku13@gmail.com>
2023-08-15 21:15:47 +0300

    first task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 2
-b = 3
-print("{:n} + {:n} = {:n}".format(a, b, add(a, b)))
+#0_add.py
+if __name__ == "__main__":
+    from add_0 import add
+    a = 2
+    b = 3
+    print("{:n} + {:n} = {:n}".format(a, b, add(a, b)))
