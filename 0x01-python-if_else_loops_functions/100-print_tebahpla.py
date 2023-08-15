1460076db42ed722fb5bda11a348c58205072b8f
Mally13 <mallymutuku13@gmail.com>
2023-08-15 15:27:38 +0300

    Task 13 - 16

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-originalrange = range(ord('a'), ord('z') + 1)
-reversedrange = list(originalrange)[::-1]
-
-for i in reversedrange:
-    if (i % 2 != 0):
-        i -= 32
-    alph = chr(i)
-    print(f"{alph}", end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1): 
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
