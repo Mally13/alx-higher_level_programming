80f6434f3bac2253a2abfc82c0394b22e14da629
Mally13 <mallymutuku13@gmail.com>
2023-08-16 14:18:26 +0300

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-# A program that does some maths and prints results
 # 1-calculation.py
 
-
-"""Performs math then prints the result"""
 if __name__ = "__main__":
     from calculator_1 import add, sub, mul, div
-
 a = 10
 b = 5
-
 print("{} + {} = {}".format(a, b, add(a, b)))
 print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} * {} = {}".format(a, b, mul(a, b)))
