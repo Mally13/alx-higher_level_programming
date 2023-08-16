f3034e3e40f871f1cbac66208f86675a00d99b4f
Mally13 <mallymutuku13@gmail.com>
2023-08-16 20:25:19 +0300

    Advanced

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# handles basic math operations
+
+if __name__ == "__main__":
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
+
+    operations = {'+': add, '-': sub, '/': div, '*':mul}
+    res = 0
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif argv[2] not in list(operations.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        res = operations[argv[2]](int(argv[1]), int(argv[3]))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
+        exit(0)
