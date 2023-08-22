411a244dcbd68ac34d22be09a31c0dde73e0bd92
Mally13 <mallymutuku13@gmail.com>
2023-08-22 13:31:55 +0300

    Python data structures

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# 5-no_c.py
+
+def no_c(my_string):
+    str_noc = []
+    for ch in my_string:
+        if (ch != 'C' and ch != 'c'):
+            str_noc.append(ch)
+    return ''.join(str_noc)
