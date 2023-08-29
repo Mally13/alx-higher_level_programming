a37de7bcf4d5c5ddd3657d06ec2ff0b5ae811da5
Mally13 <mallymutuku13@gmail.com>
2023-08-29 20:19:43 +0300

    Task 0 and Task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,6 +2,8 @@
 # 0-square.py
 
 """Define an empty class square"""
+
+
 class Square:
     """Empty class Square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-# 1-square.py
+"""
+1-square.py
+class Square that defines a square by: (based on 0-square.py)
+`Private instance attribute: size
+Instantiation with size (no type/value verification)
+"""
+
 
 class Square:
     """ defines a square by: (based on 0-square.py)"""
