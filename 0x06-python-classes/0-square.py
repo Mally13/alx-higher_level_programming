e841a3cb34f1dcbd4b5b5e4543ca13626ac82dca
Mally13 <mallymutuku13@gmail.com>
2023-08-29 15:43:29 +0300

    Task 0 -3

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 0-square.py
+
+class Square:
+    """empty class Square that defines a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 1-square.py
+
+class Square:
+    """ defines a square by: (based on 0-square.py)"""
+    def __init__(self, size):
+        self.__size = size
