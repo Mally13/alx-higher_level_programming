a2b6bbe21462b2ddb1b01f29ffd02d08f522dc21
Mally13 <mallymutuku13@gmail.com>
2023-09-06 18:21:02 +0300

    Task 0 corretion

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,17 +2,16 @@
 # 0-add_integer.py
 
 """
-Add two integers.
-Args:
-    a (int or float): The first number.
-    b (int or float, optional): The second number. Default is 98.
+Add two integers: a (int or float), b (int or float, optional Default is 98)
 Returns:
     int: The sum of a and b as an integer.
 """
 
 
 def add_integer(a, b=98):
-    """a function that adds 2 integers"""
+    """
+    a function that adds 2 ints and returns sum of a and b as an integer.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
