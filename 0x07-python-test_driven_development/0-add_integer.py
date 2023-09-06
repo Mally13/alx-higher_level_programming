bfd5f91d95c2006f5f2ca4643fec186f4bb54edd
Mally13 <mallymutuku13@gmail.com>
2023-09-06 19:00:30 +0300

    Fix Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 # 0-add_integer.py
-
-"""
-Add two integers: a (int or float), b (int or float, optional Default is 98)
+"""Add two integers:
+a (int or float), b (int or float, optional Default is 98)
 Returns:
     int: The sum of a and b as an integer.
 """
 
 
 def add_integer(a, b=98):
-    """
-    a function that adds 2 ints and returns sum of a and b asan integer.
+    """a function that adds 2 ints and
+    returns sum of a and b asan integer.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
