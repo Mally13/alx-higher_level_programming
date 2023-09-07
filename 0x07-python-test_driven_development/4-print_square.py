5c74ade364d9b71132d51fd209387553ba037093
Mally13 <mallymutuku13@gmail.com>
2023-09-07 17:06:33 +0300

    Pycode

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -19,6 +19,6 @@ def print_square(size=0):
     if size == 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
-        for j in range(size - 1):
-            print("#", end = "")
+        for j in range(size-1):
+            print("#", end="")
         print("#")
