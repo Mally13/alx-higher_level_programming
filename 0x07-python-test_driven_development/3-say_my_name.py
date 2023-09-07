a63110d56e4ab818b17fab662e7958b2d7571e94
Mally13 <mallymutuku13@gmail.com>
2023-09-07 15:52:58 +0300

    Task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,6 +11,8 @@ Args:
 
 def say_my_name(first_name="", last_name=""):
     """Prints my name"""
+    if first_name == "":
+        raise ValueError("enter first name")
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
