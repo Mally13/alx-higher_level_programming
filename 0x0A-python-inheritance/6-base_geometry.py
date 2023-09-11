43a07e1b2d380f9138f30009f5e1ef8aa048d25b
Mally13 <mallymutuku13@gmail.com>
2023-09-11 06:42:40 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+
+"""Define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """calulates area"""
+        raise Exception("area() is not implemented")
