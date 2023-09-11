a95145e09b52b5b4d35f088ca1f559f7e39d71e4
Mally13 <mallymutuku13@gmail.com>
2023-09-11 05:01:11 -0700

    Task 0

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,7 +8,7 @@ class MyList(list):
     """class MyList that inherits from list"""
     def __init__(self, *args):
         super().__init__(*args)
-    
+ 
     def print_sorted(self):
         """prints the list in an ascending sort"""
         print(sorted(self))
