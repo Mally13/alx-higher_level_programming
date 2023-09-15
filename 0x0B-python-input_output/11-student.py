246d1fd3deac66297c0b1fbdd4ed22dc899b8d16
Mally13 <mallymutuku13@gmail.com>
2023-09-15 18:08:18 +0300

    Task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -23,9 +23,9 @@ class Student:
 
     def reload_from_json(self, json):
         """replaces all attributes of the Student instance"""
-        if json["first_name"]:
+        if json["first_name"] and self.first_name:
             self.first_name = json["first_name"]
-        if json["last_name"]:
+        if json["last_name"] and self.last_name:
             self.last_name = json["last_name"]
-        if json["age"]:
+        if json["age"] and self.age:
             self.age = json["age"]
