4d2de04f8f7c30a4d1e735947f52ac0eb62ea1d0
Mally13 <mallymutuku13@gmail.com>
2023-09-18 22:39:48 +0300

    Task 1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # rectangle.py
 
 """Defines class Rectangle that inherits from Base"""
@@ -7,7 +7,14 @@ from models.base import Base
 
 class Rectangle(Base):
     """
-    Class Rectangle that inherits from Base
+    Class Rectangle that inherits from Base and has:
+        Private instance attributes,
+        each with its own public getter and setter:
+            __width -> width
+            __height -> height
+            __x -> x
+            __y -> y
+        Class constructor: def __init__(self, width, height, x=0, y=0, id=None)
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """Instatiates class Rectangle"""
