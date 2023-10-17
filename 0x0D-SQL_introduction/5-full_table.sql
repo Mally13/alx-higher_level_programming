a76ad9c1614684af3c10522efd48091c10161a44
Mally13 <mallymutuku13@gmail.com>
2023-10-17 19:01:04 +0300

    decribe table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
 -- Prints the full description of the table first_table from the database 
 -- hbtn_0c_0 in your MySQL server.
-SHOW COLUMNS FROM first_table;
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT
+FROM information_schema.COLUMNS
+WHERE AND TABLE_NAME = 'first_table';
