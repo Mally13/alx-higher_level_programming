968d42bd10763d5001627a608b0bba6f4189ecb8
Mally13 <mallymutuku13@gmail.com>
2023-11-13 15:47:56 +0300

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const { argv } = process;
 
 if (isNaN(+argv[2])) { console.log('Not a number'); } else {
   const first = parseInt(argv[2], 10);
-  console.log(first);
+  console.log(`My number: ${first}`);
 }
