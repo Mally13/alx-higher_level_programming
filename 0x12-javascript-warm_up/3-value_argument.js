ea682d0f75f397b4063f5669bb9f0cdba987d1eb
Mally13 <mallymutuku13@gmail.com>
2023-11-13 15:37:59 +0300

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-const {argv} = process;
+const { argv } = process;
 
 argv.forEach((val, index) => {
-	if (index === 1 && !argv[2])
-		console.log('No argument');
-	else if (index !== 0 && index !== 1)
-		console.log(val);
+  if (index === 1 && !argv[2]) { console.log('No argument'); } else if (index !== 0 && index !== 1) { console.log(val); }
 });
