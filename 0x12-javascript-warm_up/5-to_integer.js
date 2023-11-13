18afb0b51f4dc1d4d4ce7fa91f54f7144a27bc22
Mally13 <mallymutuku13@gmail.com>
2023-11-13 15:46:45 +0300

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
+const { argv } = process;
 
 if (isNaN(+argv[2])) { console.log('Not a number'); } else {
   const first = parseInt(argv[2], 10);
