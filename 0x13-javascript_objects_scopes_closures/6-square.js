7c78fc71eef8dc2c9f6a0304fe077500e3ab1ae6
Mally13 <mallymutuku13@gmail.com>
2023-11-14 16:29:56 +0300

    Task 6 - class Square that inherits from class Square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const Psquare = require('./5-square');
+
+class Square extends Psquare {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.width; i++) {
+      let row = '';
+      for (let j = 0; j < this.height; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
