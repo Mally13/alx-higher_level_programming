1d8ecb8d1e3a65fb1981efd642e332a22dd8c3ad
Mally13 <mallymutuku13@gmail.com>
2023-11-17 13:27:31 +0300

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute(
-        "SELECT * FROM `states` WHERE `name` ='{}'".format(argv[4])
+        "SELECT * FROM `states` WHERE BINARY `name` ='{}'".format(argv[4])
     )
     query_rows = cur.fetchall()
     for row in query_rows:
