c27e22de55ac276026c9c48f101c0d43f919a456
Mally13 <mallymutuku13@gmail.com>
2023-11-17 10:07:14 +0300

    Task 0 Documentation

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,16 +2,12 @@
 
 import MySQLdb
 from sys import argv
-
-"""Lists all states from database hbtn_0e_0_usa"""
 if __name__ == "__main__":
-    """Lists all states from database hbtn_0e_0_usa"""
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
     query_rows = cur.fetchall()
-
     for row in query_rows:
         print(row)
     cur.close()
