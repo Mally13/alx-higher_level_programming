f9be79e3b08bbf0ce7dd9fb4cdf74e4fbfe14477
Mally13 <mallymutuku13@gmail.com>
2023-11-17 10:26:26 +0300

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 
+"""
+Filters states that start with N from the database hbtn_0e_0_usa
+"""
+
 import MySQLdb
 from sys import argv
+
 if __name__ == "__main__":
+    """
+    lists all states with a name starting with N
+    (upper N) from the database hbtn_0e_0_usa
+    """
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
