d6b839d397a83075198595639419a0d9a2cce0d1
Mally13 <mallymutuku13@gmail.com>
2023-11-17 12:12:58 +0300

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-
+"""
+A script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
 import MySQLdb
 from sys import argv
+
 if __name__ == "__main__":
+    """
+    Takes in an argument and displays all values in the state table
+    """
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
