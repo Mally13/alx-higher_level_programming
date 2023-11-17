8c3d8170e8f60d3d31dadd3ea46182fc32f69dc1
Mally13 <mallymutuku13@gmail.com>
2023-11-17 13:41:41 +0300

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 
-""""""
+"""
+A script that takes in arguments and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+(safe from mysql injection)
+
+Usage:
+./1-filter_states.py <mysql username> <mysql password> \
+<database name> <state name>
+"""
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
+    """
+    A script that takes in arguments and displays all values
+    in the states table of hbtn_0e_0_usa where name matches the argument
+    (safe from mysql injection)
+    """
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
