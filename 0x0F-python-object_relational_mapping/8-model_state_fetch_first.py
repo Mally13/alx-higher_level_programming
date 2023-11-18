6db2eb234d01ed2ef08080c4fd0a8b2bfa8904c1
Mally13 <mallymutuku13@gmail.com>
2023-11-19 00:05:42 +0300

    Task 8 - First state object

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,4 +17,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     first_state = session.query(State).first()
-    print("{}: {}".format(first_state.id, first_state.name))
+    if (first_state):
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("")
