20759b8a6b6f8f422ef84d3fa3ea4af06ee981a1
Mally13 <mallymutuku13@gmail.com>
2023-11-30 17:20:11 +0300

    Task 0: Display size of body of a response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Displays the size of the body of the response
-size=$(curl -sI "$1" | awk '/Content-Length/ {print $2}' | tr -d '\r')
-echo "$size"
+echo "$(curl -sI "$1" | awk '/Content-Length/ {print $2}' | tr -d '\r')"
