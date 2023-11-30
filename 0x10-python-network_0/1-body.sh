bb0e8d02a0021a3cf47079dc59c76ec7acca3d71
Mally13 <mallymutuku13@gmail.com>
2023-11-30 18:25:25 +0300

    Task 1: Display body of a response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Displays body of a response
-status_code=$(curl -s -o /dev/null -w '%{http_code}' $1)
-if [ $status_code -eq 200 ]; then
+if [ $(curl -s -o /dev/null -w '%{http_code}' $1) -eq 200 ]; then
 	echo "$(curl -s "$1")"
 fi
