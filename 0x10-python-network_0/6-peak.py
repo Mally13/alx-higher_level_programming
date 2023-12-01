aa0163d747f6939c50b569c5ec53322c47eab724
Mally13 <mallymutuku13@gmail.com>
2023-12-01 13:43:05 +0300

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,12 +7,8 @@ def find_peak(list_of_integers):
     peak = None
     if (len(list_of_integers) != 0):
         for i in range(len(list_of_integers) - 1):
-            if (list_of_integers[i] > list_of_integers[i - 1] and
-                    list_of_integers[i] > list_of_integers[i + 1]):
-                if (peak and (list_of_integers[i] > peak)):
+            if (list_of_integers[i] >= list_of_integers[i - 1] and
+                    list_of_integers[i] >= list_of_integers[i + 1]):
+                if (peak is None or list_of_integers[i] > peak):
                     peak = list_of_integers[i]
-                else:
-                    peak = list_of_integers[i]
-            else:
-                peak = list_of_integers[i]
     return peak
