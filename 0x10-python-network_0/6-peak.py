147dffe382fb4ec44b8d287f90dcebf425a4e65f
Mally13 <mallymutuku13@gmail.com>
2023-12-01 13:33:04 +0300

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+    peak = None
+    if (len(list_of_integers) != 0):
+        for i in range(len(list_of_integers) - 1):
+            if (list_of_integers[i] > list_of_integers[i - 1] and
+                    list_of_integers[i] > list_of_integers[i + 1]):
+                if (peak and (list_of_integers[i] > peak)):
+                    peak = list_of_integers[i]
+                else:
+                    peak = list_of_integers[i]
+            else:
+                peak = list_of_integers[i]
+    return peak
