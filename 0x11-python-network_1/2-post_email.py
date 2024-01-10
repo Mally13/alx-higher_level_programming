e2d57f4a526823e95a8d94c1ffbfe937af39261c
Mally13 <mallymutuku13@gmail.com>
2024-01-10 19:16:56 +0300

    Task 2: Post an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to the passed URL with the email as
+a parameter, and displays the body of the response
+"""
+from sys import argv
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+	url = argv[1]
+	values = {'email': argv[2]}
+	data = urllib.parse.urlencode(values)
+	data = data.encode('ascii')
+	req = urllib.request.Request(url, data)
+	with urllib.request.urlopen(req) as response:
+		the_page = response.read()
