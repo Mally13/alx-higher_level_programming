4c1f080f32f59b2d1a5f46bc0edd6867b44a483d
Mally13 <mallymutuku13@gmail.com>
2024-01-30 12:53:39 +0300

    Task 0: Read contents of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,19 +4,18 @@
  * content must be read in utf-8
  */
 
-const fs = require('fs')
+const fs = require('fs');
 const { argv } = process;
-if (argv.length < 3)
-{
-	console.error('Usage: ./0-readme.js <filepath>');
-	process.exit(1);
+if (argv.length < 3) {
+  console.error('Usage: ./0-readme.js <filepath>');
+  process.exit(1);
 }
 
 const filepath = argv[2];
 fs.readFile(filepath, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(data);
-	}
-})
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
